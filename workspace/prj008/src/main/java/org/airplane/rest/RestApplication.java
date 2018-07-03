3f1abad6cd8527d47779aba0ae725b582e8099eb
a.atanasov <angelo.k.atanasov@gmail.com>
2018-07-03 18:41:10 +0300

    Add airplane service

---
+++ workspace/prj008/src/main/java/org/airplane/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package org.airplane.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+
+}
