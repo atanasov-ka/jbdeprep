af7715670bcfa212d82885e4bc7c02c94cb2fc9e
Angelo Atanasov <angelo.atanasov@windowslive.com>
2018-08-17 00:02:18 +0300

    adding verb shuffling

--- workspace/vb-frontend-2/controllers/boxController.js
+++ workspace/vb-frontend-2/controllers/boxController.js
@@ -25,8 +25,54 @@ module.exports = {
             "url": "http://localhost:8080/vb/api/play/" + playId
         }, (error, response, body) => {
             let r = JSON.parse(body);
-            console.info(r);
-            res.render('play', { play: r, body: body });
+            let indexList = [];
+            let concatHelper = function(id, s, t) {
+                Array(t).fill().map(()=>{
+                    indexList = indexList.concat({
+                        "i": id,
+                        "s": s
+                    })
+                });
+            };
+
+            let shuffle = function shuffle(array) {
+                let currentIndex = array.length, temporaryValue, randomIndex;
+                while (0 !== currentIndex) {
+                    randomIndex = Math.floor(Math.random() * currentIndex);
+                    currentIndex -= 1;
+
+                    temporaryValue = array[currentIndex];
+                    array[currentIndex] = array[randomIndex];
+                    array[randomIndex] = temporaryValue;
+                }
+
+                return array;
+            };
+
+            for (let i = 0; i < r.verbPlayList.length; ++i) {
+                if (r.verbPlayList[i].progressBack < 34) {
+                    concatHelper(i, "f", 4);
+                } else if (r.verbPlayList[i].progressBack >= 34 && r.verbPlayList[i].progressBack < 67) {
+                    concatHelper(i, "f", 3);
+                } else {
+                    // assumption >= 67
+                    concatHelper(i, "f", 2);
+                }
+            }
+
+            for (let i = 0; i < r.verbPlayList.length; ++i) {
+                if (r.verbPlayList[i].progressFront < 34) {
+                    concatHelper(i, "b", 4);
+                } else if (r.verbPlayList[i].progressFront >= 34 && r.verbPlayList[i].progressFront < 67) {
+                    concatHelper(i, "b", 3);
+                } else {
+                    // assumption >= 67
+                    concatHelper(i, "b", 2);
+                }
+            }
+            let sIndexList = shuffle(indexList);
+            console.info(sIndexList);
+            res.render('play', { play: r, body: body, playIndexes: JSON.stringify(sIndexList)});
         });
     }
 };
--- workspace/vb-frontend-2/public/js/play.js
+++ workspace/vb-frontend-2/public/js/play.js
@@ -1,26 +1,36 @@
 loadPlay = function(){
     data.box = JSON.parse(document.getElementById('boxJsonPayloadHolder').innerText.replace(/&quot;/g,'"'));
-    data.progressPercentStep = 100.0 / data.box.verbPlayList.length;
+    data.indexes = JSON.parse(document.getElementById('playIndexPayloadHolder').innerText.replace(/&quot;/g,'"'));
+    data.progressPercentStep = 100.0 / data.indexes.length;
     document.getElementById('showVerbHolder').classList.add("is-collapsed");
     document.getElementById('correctWrongHolder').classList.add("is-collapsed");
 };
 
 loadNextVerb = function() {
     data.currentIndex++;
-    if (data.box.verbPlayList.length <= data.currentIndex) {
+    if (data.indexes.length <= data.currentIndex) {
         alert("Box completed!");
         this.startPlay();
     } else {
-        document.getElementById('sideA').innerText = data.box.verbPlayList[data.currentIndex].verb.front;
-        document.getElementById('sideATranscription').innerText = data.box.verbPlayList[data.currentIndex].verb.frontTranscription;
+        data.indexToLoad = data.indexes[data.currentIndex].i;
+        data.currentSide = data.indexes[data.currentIndex].s;
+        console.info("indexTOLoad: " + data.indexToLoad);
+        if (data.currentSide === "b") {
+            console.info("Side B");
+            document.getElementById('sideA').innerText = data.box.verbPlayList[data.indexToLoad].verb.front;
+            document.getElementById('sideATranscription').innerText = data.box.verbPlayList[data.indexToLoad].verb.frontTranscription;
+        } else {
+            console.info("Side A");
+            document.getElementById('sideA').innerText = data.box.verbPlayList[data.indexToLoad].verb.back;
+            document.getElementById('sideATranscription').innerText = data.box.verbPlayList[data.indexToLoad].verb.backTranscription;
+        }
+
         document.getElementById('sideB').innerText = "-";
         document.getElementById('sideBTranscription').innerText = "-";
     }
 
     document.getElementById('showVerbHolder').classList.remove("is-collapsed");
     document.getElementById('correctWrongHolder').classList.add("is-collapsed");
-    let flush = new Audio(data.box.verbPlayList[data.currentIndex].verb.frontAudio);
-    flush.play();
     let currentProgress = data.progressPercentStep * (data.currentIndex + 1);
     document.getElementById('currentBoxProgress').setAttribute("aria-valuenow", currentProgress.toString());
     document.getElementById('currentBoxProgress').setAttribute("style", "width: " + currentProgress.toString() + "%");
@@ -42,9 +52,36 @@ startPlay = function() {
     loadNextVerb();
 };
 
+listenSideA = function() {
+    if (data.currentSide === "b") {
+        let flush = new Audio(data.box.verbPlayList[data.indexToLoad].verb.frontAudio);
+        flush.play();
+    } else {
+        let flush = new Audio(data.box.verbPlayList[data.indexToLoad].verb.backAudio);
+        flush.play();
+    }
+};
+
+listenSideB = function() {
+    if (data.currentSide === "b") {
+        let flush = new Audio(data.box.verbPlayList[data.indexToLoad].verb.backAudio);
+        flush.play();
+    } else {
+        let flush = new Audio(data.box.verbPlayList[data.indexToLoad].verb.frontAudio);
+        flush.play();
+    }
+};
+
 showVerb = function() {
-    document.getElementById('sideB').innerText = data.box.verbPlayList[data.currentIndex].verb.back;
-    document.getElementById('sideBTranscription').innerText = data.box.verbPlayList[data.currentIndex].verb.backTranscription;
+    if (data.currentSide === "b") {
+        console.info("Side B");
+        document.getElementById('sideB').innerText = data.box.verbPlayList[data.indexToLoad].verb.back;
+        document.getElementById('sideBTranscription').innerText = data.box.verbPlayList[data.indexToLoad].verb.backTranscription;
+    } else {
+        console.info("Side A");
+        document.getElementById('sideB').innerText = data.box.verbPlayList[data.indexToLoad].verb.front;
+        document.getElementById('sideBTranscription').innerText = data.box.verbPlayList[data.indexToLoad].verb.frontTranscription;
+    }
 
     document.getElementById('showVerbHolder').classList.add("is-collapsed");
     document.getElementById('correctWrongHolder').classList.remove("is-collapsed");
@@ -53,6 +90,9 @@ showVerb = function() {
 let data = {
     progressPercentStep: null,
     currentIndex: null,
-    box: null
+    box: null,
+    indexes: null,
+    indexToLoad: null,
+    currentSide: null
 };
 
