447d4fcc8efa3c5260e8eaddfbf9a2c5495fc79c
Angelo Atanasov <angelo.atanasov@windowslive.com>
2018-08-23 21:49:13 +0300

    add logout functionality

--- workspace/vb-frontend-2/routes/index.js
+++ workspace/vb-frontend-2/routes/index.js
@@ -18,6 +18,11 @@ function isAuthenticated(req, res, next) {
 }
 
 router.post('/auth', authController.authenticate);
+router.get('/logout', function(req, res, next) {
+    req.session.user = null;
+    req.session.authorization = null;
+    res.redirect("/");
+});
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Verbbox Login' });
 });
