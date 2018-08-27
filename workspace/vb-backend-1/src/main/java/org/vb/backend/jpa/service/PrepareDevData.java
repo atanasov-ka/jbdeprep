51bf27863c2255e9215135cee3c149b7da5932cf
Angelo Atanasov <angelo.atanasov@windowslive.com>
2018-08-28 00:02:03 +0300

    add auto user registration

---
+++ workspace/vb-backend-1/src/main/java/org/vb/backend/jpa/service/PrepareDevData.java
@@ -0,0 +1,26 @@
+package org.vb.backend.jpa.service;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+/**
+ * Session Bean implementation class PrepareDevData
+ */
+@Singleton
+@LocalBean
+public class PrepareDevData {
+
+	@EJB
+	private UserService userService;
+	
+	@PostConstruct
+	public void init() {
+		userService.register("admin", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", false);
+		userService.register("user1", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", false);
+		userService.register("user2", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", false);
+		userService.register("user3", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", false);
+		userService.register("user4", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", false);
+	}    
+}
