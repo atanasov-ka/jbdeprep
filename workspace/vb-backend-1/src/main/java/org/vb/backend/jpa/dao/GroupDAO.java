9690c9af78200e059ca838e5d8943d25a2b14abd
atanasov-ka <angelo.k.atanasov@gmail.com>
2019-05-05 21:43:40 +0000

    Add getAllGroups dao method

--- workspace/vb-backend-1/src/main/java/org/vb/backend/jpa/dao/GroupDAO.java
+++ workspace/vb-backend-1/src/main/java/org/vb/backend/jpa/dao/GroupDAO.java
@@ -3,15 +3,28 @@ package org.vb.backend.jpa.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.vb.backend.jpa.pojos.BoxCategory;
 
 @Stateless
 public class GroupDAO {
 
+	@PersistenceContext(unitName="vb-backend-1")
+	private EntityManager entityManager;
+
 	public List<BoxCategory> getAllGroups(String username, boolean isAdmin) {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<BoxCategory> allCategoryQuery;
+		if (isAdmin) {
+			allCategoryQuery = entityManager.createQuery("select b from BoxCategory b order by b.name", BoxCategory.class);
+		} else {
+			allCategoryQuery = entityManager.createQuery("select b from BoxCategory b where b.user.username = :username order by b.name", BoxCategory.class);
+			allCategoryQuery.setParameter("username", username);
+		}
+
+		return allCategoryQuery.getResultList();
 	}
 
 	public BoxCategory getGroup(String username, boolean isAdmin, Long groupId) {
