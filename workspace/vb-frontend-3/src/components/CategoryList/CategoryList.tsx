8440792ca7291d12bfda91949d3fb87f05fd8552
atanasov-ka <angelo.k.atanasov@gmail.com>
2019-05-06 18:13:20 +0000

    Implement the adding category

--- workspace/vb-frontend-3/src/components/CategoryList/CategoryList.tsx
+++ workspace/vb-frontend-3/src/components/CategoryList/CategoryList.tsx
@@ -14,6 +14,7 @@ import Category from "../Category/Category";
 
 type Categories = {
     dialogOpen: boolean,
+    newCategoryName: string,
     items : Array<{
         "groupId": number,
         "groupName": string,
@@ -25,12 +26,10 @@ class CategoryList extends React.Component<RouteComponentProps, Categories> {
     constructor(props){
         super(props);
         this.componentDidMount = this.componentDidMount.bind(this);
-        this.state = {dialogOpen:false, items:[]};
+        this.state = {dialogOpen:false, newCategoryName:"", items:[]};
     }
 
     componentDidMount() {
-
-        // this.state = { list:[{id:1, name: "a"}] };
         let url = 'http://localhost:8081/vb/api/category';
         fetch(url, { method: "GET", headers: {
             'Content-Type': 'application/json',
@@ -55,6 +54,31 @@ class CategoryList extends React.Component<RouteComponentProps, Categories> {
         this.setState({ dialogOpen: false });
     };
 
+    setNewCategoryName = (event) => {
+        this.setState({newCategoryName: event.target.value});
+    };
+
+    handleCreate = () => {
+        let url = 'http://localhost:8081/vb/api/category';
+        fetch(url, { method: "POST", headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + localStorage.getItem("authToken")
+            }, body: JSON.stringify({groupName:this.state.newCategoryName})})
+            .then(response => response.text())
+            .then(json => {
+                console.log(JSON.parse(json));
+                let newCategory = JSON.parse(json);
+                let elems = this.state.items;
+                elems.push(newCategory);
+                this.setState({ items:  elems});
+                this.setState({ dialogOpen: false });
+            })
+            .catch(error => {
+                //this.setState({error: error});
+                console.error(error);
+            });
+    };
+
     render() {
         return <div>
             {
@@ -70,13 +94,12 @@ class CategoryList extends React.Component<RouteComponentProps, Categories> {
                 </CardActions>
             </Card>
             <Dialog open={this.state.dialogOpen} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Create new Category</DialogTitle>
+                <DialogTitle id="form-dialog-title">New category</DialogTitle>
                 <DialogContent>
-                    <TextField autoFocus margin="dense" id="name" label="Category name" type="text" fullWidth />
+                    <TextField autoFocus margin="dense" id="name" label="Name" type="text" onChange={this.setNewCategoryName} fullWidth />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={this.handleClose} color="primary">Cancel</Button>
-                    <Button onClick={this.handleClose} color="primary">Create</Button>
+                    <Button onClick={this.handleCreate} color="primary">Create</Button>
                 </DialogActions>
             </Dialog>
         </div>
